f577825d2dcaf40191b5d1c31b7cb22fe0a0c1f1
Vinicius <vinicius.giroti@gmail.com>
2024-11-30 16:26:19 -0300

    Created a function to initialize the automata and their transitions called 'initialize'. Added the initialization of the paths starting in 'C' and 'D' in which turns are not permitted at traffic lights

--- main.c
+++ main.c
@@ -1,10 +1,17 @@
 #include "automaton.h"
 #include <stdio.h>
 
+void initialize( AutomatoPilha **at, Transicao ***tr );
+
 int main(){
-    // Construção dos automatos com pilha (Controle do tráfego)
     AutomatoPilha** at = malloc( 11 * sizeof(AutomatoPilha*) );
     Transicao ***tr = malloc(11*sizeof(Transicao*));
+    initialize( at, tr );
+
+}
+
+void initialize( AutomatoPilha **at, Transicao ***tr ) {
+    // Construção dos automatos com pilha (Controle do tráfego)
     for( int i = 0; i < 2; i++ ) {
         at[i] = buildAuto(6);
         at[i]->estInicial=0;
@@ -77,14 +84,10 @@ int main(){
         inserirTransicao(at[1],2,tr[1][i]);
     for(int i = 13; i < 18; i++)
         inserirTransicao(at[1],3,tr[1][i]);
-
-    char *c2 = "NNSOO\0";
-    char *c3 = "NNSLL\0";
-    printf("%i\n",avaliar(c2,at[0], 2));
-    printf("%i\n", avaliar(c3,at[1], 3));
-    printf("AAAAAAAAAAAAAAA nao dei segfault!!!!!!!!!!!!!!!!!!!!!!\n");
     
     // Construção dos caminhos dos carros (AFDs)
+
+    // Construção dos caminhos a partir de 'A' e 'B', sem curvas nos pontos do semáforo
     for( int i = 2; i < 4; i++ ) {
         at[i] = buildAuto(8);
         at[i]->estInicial=0;
@@ -111,4 +114,39 @@ int main(){
     char *b = "SSSNNNE\0";
     printf("%i\n",avaliar(a,at[2], 0));
     printf("%i\n", avaliar(b,at[3], 0));
+
+    // Construção do caminho a partir de 'C', sem curvas nos pontos do semáforo
+    at[4] = buildAuto(5);
+    at[4]->estInicial = 0;
+    at[4]->estFinal[4] = 1;
+    tr[4] = malloc(4*sizeof(Transicao*));
+    tr[4][0] = buildTrans(1,'L','&','&');
+    tr[4][1] = buildTrans(2,'L','&','&');
+    tr[4][2] = buildTrans(3,'L','&','&');
+    tr[4][3] = buildTrans(4,'E','&','&');
+    inserirTransicao(at[4],0,tr[4][0]);
+    inserirTransicao(at[4],1,tr[4][1]);
+    inserirTransicao(at[4],2,tr[4][2]);
+    inserirTransicao(at[4],2,tr[4][3]);
+    char *c = "LLL\0";
+    printf("%i\n",avaliar(c,at[4], 0));
+    
+    // Construção do caminho a partir de 'D', sem curvas nos pontos do semáforo
+    at[5] = buildAuto(6);
+    at[5]->estInicial = 0;
+    at[5]->estFinal[4] = 1;
+    at[5]->estFinal[5] = 1;
+    tr[5] = malloc(5*sizeof(Transicao*));
+    tr[5][0] = buildTrans(1,'O','&','&');
+    tr[5][1] = buildTrans(2,'O','&','&');
+    tr[5][2] = buildTrans(3,'O','&','&');
+    tr[5][3] = buildTrans(4,'E','&','&');
+    tr[5][4] = buildTrans(5,'E','&','&');
+    inserirTransicao(at[5],0,tr[5][0]);
+    inserirTransicao(at[5],1,tr[5][1]);
+    inserirTransicao(at[5],1,tr[5][3]);
+    inserirTransicao(at[5],2,tr[5][2]);
+    inserirTransicao(at[5],2,tr[5][4]);
+    char *d = "OOE\0";
+    printf("%i\n",avaliar(d,at[5], 0));
 }
